07721d4858e19ed33ca80e09462acf7ef4d2b24e
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-12-04 16:33:23 -0500

    Update 01A_inverse-captcha.js

--- 01A_inverse-captcha.js
+++ 01A_inverse-captcha.js
@@ -1,1 +1,5 @@
-
+function inverseCaptcha_clever(input){
+  return input.split("")
+  .map(val => parseInt(val,10))
+  .reduce((acc, val, i, arr) => val === (arr[i+1] === undefined ? arr[0]:arr[i+1]) ? acc+val : acc, 0);
+}
