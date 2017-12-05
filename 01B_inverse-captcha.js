9db9b5fc41041894dad5873c57a753da1adf71ce
Nick Palenchar <nickpalenchar@users.noreply.github.com>
2017-12-05 13:32:05 -0500

    Update 01B_inverse-captcha.js

--- 01B_inverse-captcha.js
+++ 01B_inverse-captcha.js
@@ -7,9 +7,7 @@ function inverseCaptcha2_clever(input){
   return input.split("")
   .map(val => parseInt(val,10))
   .reduce((acc, val, i, arr) => {
-    console.log("accu is at ", acc);
     let compare = (i + arr.length/2) % arr.length;
-    console.log("val ", val, "compare ", compare);
     return val === arr[compare] ? acc+val : acc;
     }
     ,0);
